4707217c5c108a85e8449814102d7612e3d2d322
joshdev <joshuaajayi012@gmail.com>
2023-01-18 12:34:11 +0100

    ready to deploy

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # third parties
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # ......
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 
